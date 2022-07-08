dd7f4e722c4823b92f397e67dc5a55a389533805
Ran <xyfwdy@gmail.com>
2022-07-08 16:43:11 +0800

    20220708

--- src/main/java/cn/ranmc/CheckHttpHandler.java
+++ src/main/java/cn/ranmc/CheckHttpHandler.java
@@ -2,17 +2,28 @@ package cn.ranmc;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.net.URLDecoder;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CheckHttpHandler implements HttpHandler {
+
+    private String key = "";
+
+    public CheckHttpHandler(String key) {
+        this.key = key;
+    }
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
@@ -27,11 +38,11 @@ public class CheckHttpHandler implements HttpHandler {
             result += new String(bytes,0,len);
         }
         JSONObject obj = new JSONObject(result);
-
         if (obj.has("method") && obj.getString("method").equals("newmsg")) {
             JSONObject data = obj.getJSONObject("data");
             String msg = data.getString("msg");
             if(msg.contains("查服 ")) {
+                String wxid = data.getString("fromid");
                 msg = msg.replace("查服 ", "");
                 String ip = msg;
                 int port = 25565;
@@ -39,12 +50,27 @@ public class CheckHttpHandler implements HttpHandler {
                     port = Integer.parseInt(ip.split(":")[1]);
                     ip = ip.split(":")[0];
                 }
-                long time = System.currentTimeMillis();
-                JSONObject ping = new JSONObject(new MinecraftPing().getPing(new MinecraftPingOptions().setHostname(ip).setPort(port).setTimeout(5000)));
-                System.out.println("查询结果\n地址: " + msg +
-                        "\n人数: " + ping.getJSONObject("players").getInt("online") + "/" + ping.getJSONObject("players").getInt("max") +
-                        "\n版本: " + ping.getJSONObject("version").getString("name") +
-                        "\n延迟: " + (System.currentTimeMillis() - time) + "ms");
+                JSONObject ping = new JSONObject(new MinecraftPing().getPing(new MinecraftPingOptions().setHostname(ip).setPort(port)));
+                String text = "查询结果\n地址:" + msg +
+                        "\n人数:" + ping.getJSONObject("players").getInt("online") + "/" + ping.getJSONObject("players").getInt("max") +
+                        "\n版本:" + ping.getJSONObject("version").getString("name");
+                System.out.println("来自 " + data.getString("fromid") + "的查询");
+                System.out.println(text);
+                HttpPost post = new HttpPost("http://127.0.0.1:8203/api?json&key=" + key);
+                try {
+                    List<BasicNameValuePair> list = new ArrayList<>();
+                    list.add(new BasicNameValuePair("method", "sendText"));
+                    list.add(new BasicNameValuePair("wxid", wxid));
+                    list.add(new BasicNameValuePair("msg", text));
+                    list.add(new BasicNameValuePair("atid", ""));
+                    list.add(new BasicNameValuePair("pid", "0"));
+                    post.setEntity(new UrlEncodedFormEntity(list, "UTF-8"));
+                    HttpResponse response = HttpClients.createDefault().execute(post);
+                    HttpEntity entity = response.getEntity();
+                    System.out.println(EntityUtils.toString(entity, "UTF-8"));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
             os.write("ok".getBytes("UTF-8"));
         } else {
--- src/main/java/cn/ranmc/Main.java
+++ src/main/java/cn/ranmc/Main.java
@@ -2,23 +2,30 @@ package cn.ranmc;
 
 import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.spi.HttpServerProvider;
+import lombok.Getter;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.Scanner;
 
 public class Main {
+    @Getter
+    private static String key = "";
 
     public static void main(String[] args) {
         HttpServer httpserver;
         int port = 8086;
 
         System.out.println("查询我的世界服务器 By阿然");
-        System.out.println("QQ 2263055528");
+        System.out.println("作者QQ 2263055528");
+        System.out.println("请输入key用于回复信息");
+        Scanner scanner = new Scanner(System.in);
+        key = scanner.next();
         try {
             HttpServerProvider provider = HttpServerProvider.provider();
             httpserver = provider.createHttpServer(new InetSocketAddress(port), 100);
 
-            httpserver.createContext("/check",new CheckHttpHandler());
+            httpserver.createContext("/check",new CheckHttpHandler(key));
             httpserver.setExecutor(null);
             httpserver.start();
 
